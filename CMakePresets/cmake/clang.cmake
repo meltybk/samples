f0eb448f374ac617362ef3d8ca7dd9755e3ac936
melty <11942219+meltybk@users.noreply.github.com>
2021-05-08 05:06:56 +0900

    Use CMAKE_MAKE_PROGRAM variable.

--- CMakePresets/cmake/clang.cmake
+++ CMakePresets/cmake/clang.cmake
@@ -1,5 +1,5 @@
-set(CMAKE_C_COMPILER /usr/bin/clang-11)
-set(CMAKE_CXX_COMPILER /usr/bin/clang-11)
+set(CMAKE_C_COMPILER /bin/clang-11)
+set(CMAKE_CXX_COMPILER /bin/clang-11)
 
 set(CMAKE_CXX_FLAGS "-Wall")
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" )
